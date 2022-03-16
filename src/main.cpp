d6f6528a1baa242149b8800a49091c86317335aa
Pranav Shridhar <pranavmodx@gmail.com>
2022-03-16 12:24:25 +0530

    add pugixml as static lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+#include <iostream>
+
+#include "pugixml/pugixml.hpp"
+#include "SFML/Graphics.hpp"
+
 int main() {
+	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+	std::cout << desktop.height << std::endl;
+
+	std::string my_xml = "<Person>XYZ</Person>";
+
+	pugi::xml_document doc;
+	pugi::xml_parse_result result = doc.load_string(my_xml.data());
+	if (!result)
+        return -1;
+
+	std::cout << doc.first_child().name() << std::endl;
+
 	return 0;
 }
