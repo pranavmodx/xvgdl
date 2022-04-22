8b3f0bded9501c5d110669e9e6d2151f0a0cd101
Pranav Shridhar <pranavmodx@gmail.com>
2022-04-22 11:09:12 +0530

    remove dynamic libs from repo, reorganize dirs

---
+++ include/parser/Tokens.hpp
@@ -0,0 +1,10 @@
+const std:string 
+GAME_DEFINITION = "gameDefinition",
+LAYOUT = "layout",
+MAP = "map",
+CONTROLS = "controls",
+PLAYERS = "players",
+OBJECTS = "objects",
+EVENTS = "events",
+RULES = "rules",
+END_CONDITIONS = "endConditions";
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,107 @@
 #include <iostream>
-
 #include "pugixml/pugixml.hpp"
 #include "SFML/Graphics.hpp"
 
-int main() {
-	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-	std::cout << desktop.height << std::endl;
+#include <xercesc/util/XMLString.hpp>
+#include <xercesc/parsers/XercesDOMParser.hpp>
+#include <xercesc/framework/LocalFileInputSource.hpp>
+#include <xercesc/sax/ErrorHandler.hpp>
+#include <xercesc/sax/SAXParseException.hpp>
+#include <xercesc/validators/common/Grammar.hpp>
 
-	std::string my_xml = "<Person>XYZ</Person>";
+XERCES_CPP_NAMESPACE_USE
 
-	pugi::xml_document doc;
-	pugi::xml_parse_result result = doc.load_string(my_xml.data());
-	if (!result)
-        return -1;
+class WStr
+{
+    private:
+        XMLCh*  wStr;
 
-	std::cout << doc.first_child().name() << std::endl;
+    public:
+        WStr(const char* str)
+        {
+            wStr = XMLString::transcode(str);
+        }
 
-	return 0;
+        ~WStr()
+        {
+            XMLString::release(&wStr);
+        }
+
+        operator const XMLCh*() const
+        {
+            return wStr;
+        }
+};
+
+class ParserErrorHandler : public ErrorHandler
+{
+    private:
+        void reportParseException(const SAXParseException& ex)
+        {
+            char* msg = XMLString::transcode(ex.getMessage());
+            fprintf(stderr, "at line %llu column %llu, %s\n", 
+                    ex.getLineNumber(), ex.getColumnNumber(), msg);
+            XMLString::release(&msg);
+        }
+
+    public:
+        void warning(const SAXParseException& ex)
+        {
+            reportParseException(ex);
+        }
+
+        void error(const SAXParseException& ex)
+        {
+            reportParseException(ex);
+        }
+
+        void fatalError(const SAXParseException& ex)
+        {
+            reportParseException(ex);
+        }
+
+        void resetErrors()
+        {
+        }
+};
+
+void ValidateSchema(const char* schemaFilePath, const char* xmlFilePath)
+{
+    XercesDOMParser domParser;
+    if (domParser.loadGrammar(schemaFilePath, Grammar::SchemaGrammarType) == NULL)
+    {
+        fprintf(stderr, "couldn't load schema\n");
+        return;
+    }
+
+    ParserErrorHandler parserErrorHandler;
+
+    domParser.setErrorHandler(&parserErrorHandler);
+    domParser.setValidationScheme(XercesDOMParser::Val_Auto);
+    domParser.setDoNamespaces(true);
+    domParser.setDoSchema(true);
+    domParser.setValidationConstraintFatal(true);
+
+    domParser.parse(xmlFilePath);
+    if (domParser.getErrorCount() == 0)
+        printf("XML file validated against the schema successfully\n");
+    else
+        printf("XML file doesn't conform to the schema\n");
+}
+
+int main(int argc, const char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("SchemaValidator <schema file> <xml file>\n");
+        return 0;
+    }
+
+    XMLPlatformUtils::Initialize();
+
+    ValidateSchema(argv[1], argv[2]);
+
+    XMLPlatformUtils::Terminate();
+
+    return 0;
 }
